09aba359127eecc12ed448ac93435f312112c26d
nignanthomas <nignanthomas@gmail.com>
2018-09-29 10:30:14 +0300

    add functio reload(), turnPlayer(), winner(), prototype scorePlus(); download check image; global scope playerOne and playerTwo objects; resize check img

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,61 @@
 //Business Logic
 
-var Player = function(name, score){
+var Player = function(name, score, status){
   this.name = name;
   this.score = score;
+  this.status = status;
 }
+
+Player.prototype.scorePlus = function(){
+  this.score += roundTotal;
+}
+
+var playerOne = {};
+var playerTwo = {};
+
+//function that throws dice
 var rolled = 0;
 var roundTotal = 0;
 var roll = function(){
   rolled = Math.floor(Math.random()*6) + 1;
-  if (rolled != 1)
-  roundTotal += rolled
+  $("#new-dice").remove();
+  $(".dice-img").append("<img id='new-dice' src='images/dice"+rolled+".png' alt='Dice "+rolled+"'>");
+  if (rolled != 1){
+    roundTotal += rolled
+  } else {
+    roundTotal = 0;
+    turnPlayer();
+  }
 }
+
+//function to change players turns
+var turnPlayer = function(){
+  if (playerOne.status == "enabled") {
+    playerOne.status = "disabled";
+    playerTwo.status = "enabled";
+    $(".one-turn").hide();
+    $(".two-turn").show();
+  } else if (playerTwo.status == "enabled") {
+    playerOne.status = "enabled";
+    playerTwo.status = "disabled";
+    $(".two-turn").hide();
+    $(".one-turn").show();
+  }
+}
+
+// function Winner
+var winner = function(){
+  if (playerOne.score >= 100){
+    $(".dice-img").text("!!!" + playerOne.name + "  WINS !!!")
+    $("#roll").prop("disabled", true);
+    $("#hold").prop("disabled", true);
+  } else if (playerTwo.score >= 100){
+    $(".dice-img").text("!!!" + playerTwo.name + "  WINS !!!")
+    $("#roll").prop("disabled", true);
+    $("#hold").prop("disabled", true);
+  }
+}
+
 
 //User Interface
 
@@ -19,18 +64,40 @@ $(document).ready(function(){
     event.preventDefault();
     playerOneName = $("#player-one").val();
     playerTwoName = $("#player-two").val();
-    var playerOne = new Player(playerOneName, 0);
-    var playerTwo = new Player(playerTwoName, 0);
+    playerTwo = new Player(playerTwoName, 0, "disabled");
+    playerOne = new Player(playerOneName, 0, "enabled");
 
     $(".login").hide();
     $(".pig-game").show();
     $(".player-one-name").text(playerOne.name);
     $(".player-two-name").text(playerTwo.name);
+    $(".two-turn").hide();
+    $(".one-turn").show();
 
     $("#roll").click(function(){
       roll();
-      console.log(rolled);
-      console.log(roundTotal);
-    })
-  })
-})
+      $("#round-total").text(roundTotal);
+    }) // end roll click
+
+    $("#hold").click(function(){
+      //must add commit remove img dice if click hold
+      $("#new-dice").remove();
+      if (playerOne.status == "enabled") {
+        playerOne.scorePlus();
+        $(".player-one-score").text(playerOne.score);
+        winner();
+        turnPlayer();
+      } else if (playerTwo.status == "enabled"){
+        playerTwo.scorePlus();
+        $(".player-two-score").text(playerTwo.score);
+        winner();
+        turnPlayer();
+      }
+      roundTotal = 0;
+    }) // end hold click
+  }) // end submit start
+}) // end ready
+
+function reload() {
+  location.reload();
+}
